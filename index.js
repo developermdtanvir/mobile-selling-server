5d69f158daf4458e60e09914000c300ce9d87d6b
devmdtanvir <officialtanvir@hotmail.com>
2023-03-04 14:53:49 +0600

    service find oparetion is running

--- index.js
+++ index.js
@@ -8,7 +8,9 @@
 // Dependencies
 const express = require("express");
 const cors = require("cors");
-const services = require("./routes/service");
+// const services = require("./routes/service");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
 // object module scaffolding
 
@@ -23,12 +25,51 @@ const app = express();
 // middleware
 app.use(cors());
 
-// service api
-app.get("/services", services.getService);
+// database
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.338egrb.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const serviceCollection = client.db(process.env.DB_USER).collection("service");
 
 app.get("/", (req, res) => {
   res.send("My Mongodb Server is running");
 });
+
+client.connect((err) => {
+  const collection = client.db(process.env.DB_USER).collection("products");
+
+  // perform actions on the collection object
+  const product = { name: "Modhu", price: 120, quantity: 30 };
+  collection.insertOne(product).then((result) => {
+    console.log("One product added");
+  });
+  console.log("database connected");
+});
+
+async function run() {
+  try {
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // app.get("/service/:id", async (req, res) => {
+    //   const { id } = req.params;
+    //   const query = { _id: new ObjectId(id) };
+    //   console.log(query);
+    // });
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => console.log(`listen port ${port}`));
 
--- routes/service.js
+++ routes/service.js
@@ -6,6 +6,7 @@
  */
 
 // Dependencies
+const express = require("express");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 
@@ -14,17 +15,24 @@ const service = {};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.338egrb.mongodb.net/?retryWrites=true&w=majority`;
 
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
-});
+const app = express();
 
-console.log(uri);
+const client = new MongoClient(uri);
 
-service.getService = (req, res) => {
-  res.send("Service API Hear");
-};
-
+const db = client.db("mobile-selling");
+const serviceCollection = db.collection("service");
+async function run() {
+  try {
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
 // module export
-module.exports = service;
+module.exports = run;
